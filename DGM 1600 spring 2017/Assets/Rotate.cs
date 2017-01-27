044138e95636ae8e4e2793173b44356264c0a89c
Unknown <kittbeesley@gmail.com>
2017-01-26 20:02:48 -0700

    made items move

---
+++ DGM 1600 spring 2017/Assets/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	public int number = 12;
+	public int rotSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(number);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	transform.Rotate(0,4,3);
+	//transform.Rotate(Vector3.up,4*Time.deltaTime);
+	}
+}
