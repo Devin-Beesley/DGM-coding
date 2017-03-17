e0a85f07dcd67cc132773f9411622a20d7bfa654
Unknown <kittbeesley@gmail.com>
2017-03-16 18:56:45 -0600

    review array coding and mesh with level load string and solo learn

---
+++ DGM 1600 spring 2017/Assets/PauseArray.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseArray : MonoBehaviour {
+
+	public string[] menu= {"Restart", "Inventory", "Difficulty", "Quit", "Exit"};
+	
+}
