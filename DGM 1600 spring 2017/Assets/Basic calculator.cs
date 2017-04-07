e08d47a9725a8980104f3a8183d5b6dd301baaf2
Unknown <kittbeesley@gmail.com>
2017-04-06 18:58:22 -0600

    need to fix compiler errors for simple calculator

---
+++ DGM 1600 spring 2017/Assets/Basic calculator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Basiccalculator : MonoBehaviour {
+
+
+public InputField input1;
+public InputField input2;
+public Text result;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+public void Proses(){
+
+	int variable;
+	variable = int.Parse(input1.text) + int.Parse(input2.text);
+
+	result.text = varible.ToString();
+
+	}
+}
